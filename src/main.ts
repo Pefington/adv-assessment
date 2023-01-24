38af2b0ffc97b62cd46b59f632dd27bc59e5b0e5
Pierre-François Salmand <maabaa@gmail.com>
2023-01-24 13:50:41 +0700

    Achieve initial functional version

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ const ROUNDING_STEP = 5;
 
 type Basket = Product[];
 type PriceInCents = number;
+type TotalInCents = PriceInCents;
 type Quantity = number;
 type DisplayName = string;
 
@@ -22,31 +23,44 @@ interface Product {
 }
 
 interface Invoice {
-  products: Array<[Quantity, DisplayName, PriceInCents]>;
+  products: Array<[Quantity, DisplayName, PriceInCents, TotalInCents]>;
   taxesAmount: PriceInCents;
-  totalPrice: PriceInCents;
+  basketPrice: PriceInCents;
 }
 
-// export function displayInvoice( invoice: Invoice ): void {
-//   console.log(invoice)
-// }
+export function printInvoice(basket: Basket): void {
+  const invoice = generateInvoice(basket);
+  invoice.products.forEach((product) => {
+    const [quantity, name, price, totalProductPrice] = product;
+    console.log(
+      `* ${quantity} ${name} à ${formatFR(price)}\xa0€ : ${formatFR(totalProductPrice)}\xa0€ TTC`
+    );
+  });
+  console.log(`\nMontant des taxes : ${formatFR(invoice.taxesAmount)}\xa0€`);
+  console.log(`Total : ${formatFR(invoice.basketPrice)}\xa0€\n\n`);
+}
+
+export function formatFR(price: PriceInCents): string {
+  return (price / 100).toFixed(2).replace('.', ',');
+}
 
 export function generateInvoice(basket: Basket): Invoice {
   const invoice: Invoice = {
     products: [],
     taxesAmount: 0,
-    totalPrice: 0,
+    basketPrice: 0,
   };
   basket.forEach((product) => {
     const isSingle = product.quantity === 1;
     invoice.products.push([
       product.quantity,
       isSingle ? product.nameSingular : product.namePlural,
-      calculateProductPrice(product) * product.quantity
+      product.priceInCents,
+      calculateProductPrice(product) * product.quantity,
     ]);
   });
   invoice.taxesAmount = calculateBasketTaxes(basket);
-  invoice.totalPrice = calculateBasketTotal(basket);
+  invoice.basketPrice = calculateBasketTotal(basket);
   return invoice;
 }
 
@@ -74,10 +88,12 @@ export function calculateProductPrice(product: Product): PriceInCents {
 }
 
 export function calculateProductTax(product: Product): PriceInCents {
-  const tax = applyRoundingStep(product.priceInCents * product.taxRate / 100);
+  const tax = applyRoundingStep((product.priceInCents * product.taxRate) / 100);
   if (!product.isImported) return tax;
 
-  const importTax = applyRoundingStep(product.priceInCents * IMPORT_TAX_RATE / 100);
+  const importTax = applyRoundingStep(
+    (product.priceInCents * IMPORT_TAX_RATE) / 100
+  );
   return tax + importTax;
 }
 
@@ -171,4 +187,6 @@ export const basket3: Basket = [
   },
 ];
 
-// calculateProductPrice(basket3[0]);
+printInvoice(basket1);
+printInvoice(basket2);
+printInvoice(basket3);
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vitest } from 'vitest';
 import {
   basket3,
   applyRoundingStep,
@@ -7,6 +7,8 @@ import {
   calculateBasketTaxes,
   calculateBasketTotal,
   generateInvoice,
+  printInvoice,
+  formatFR,
 } from '../src/main.ts';
 
 const other1899 = basket3[1];
@@ -56,13 +58,42 @@ describe('generateInvoice', () => {
   it('should return an object with the desired output data', () => {
     expect(generateInvoice(basket3)).toEqual({
       products: [
-        [2, 'flacons de parfum importés', 6998],
-        [1, 'flacon de parfum', 2279],
-        [3, 'boîtes de pilules contre la migraine', 2925],
-        [2, 'boîtes de chocolats importées', 2370],
+        [2, 'flacons de parfum importés', 2799, 6998],
+        [1, 'flacon de parfum', 1899, 2279],
+        [3, 'boîtes de pilules contre la migraine', 975, 2925],
+        [2, 'boîtes de chocolats importées', 1125, 2370],
       ],
       taxesAmount: 1900,
-      totalPrice: 14572,
+      basketPrice: 14572,
     });
+  });
+});
+
+describe('formatFR', () => {
+  it('should show an integer to two decimal places, with a comma', () => {
+    expect(formatFR(1900)).toStrictEqual('19,00');
+  });
+});
+
+describe('printInvoice', () => {
+  it('should console.log a formatted invoice', () => {
+    const logSpy = vitest.spyOn(global.console, 'log');
+    printInvoice(basket3);
+    expect(logSpy).toHaveBeenCalledWith(
+      '* 2 flacons de parfum importés à 27,99\xa0€ : 69,98\xa0€ TTC'
+    );
+    expect(logSpy).toHaveBeenCalledWith(
+      '* 1 flacon de parfum à 18,99\xa0€ : 22,79\xa0€ TTC'
+    );
+    expect(logSpy).toHaveBeenCalledWith(
+      '* 3 boîtes de pilules contre la migraine à 9,75\xa0€ : 29,25\xa0€ TTC'
+    );
+    expect(logSpy).toHaveBeenCalledWith(
+      '* 2 boîtes de chocolats importées à 11,25\xa0€ : 23,70\xa0€ TTC'
+    );
+    expect(logSpy).toHaveBeenCalledWith('\nMontant des taxes : 19,00\xa0€');
+    expect(logSpy).toHaveBeenCalledWith('Total : 145,72\xa0€\n\n');
+
+    logSpy.mockClear();
   });
 });
